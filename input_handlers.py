a6d9588cd93486dbd8ac3edae743116fdead3acf
NekkidBear <52714446+NekkidBear@users.noreply.github.com>
2019-07-15 14:19:15 -0500

    Versions from work computer

--- input_handlers.py
+++ input_handlers.py
@@ -1,24 +1,21 @@
-import tcod as libtcod
+import libtcodpy as libtcod
 
 def handle_keys(key):
-  #Movement Keys
-  if key.vk == libtcod.KEY_UP:
-    return {'move': (0,-1)}
-  elif key.vk == libtcod.KEY_DOWN:
-    return {'move': (0,1)}
-  elif key.vk == libtcod.KEY_LEFT:
-    return {'move': (-1,0)}
-  elif keuy.vk == libtcod.KEY_RIGHT:
-    return {'move': (1,0)}
-    
-  if key.vk == libtcod.KEY_ENTER and key.lalt:
-    #Alt+Enter: toggle fullscreen
-    return {'fullscreen': True}
-    
-  elif key.vk == libtcod.KEY_ESCAPE:
-    #exit the game
-    return {'exit': True}
-    
-  #No key pressed
-  return {}
-  
+    #movement keys
+    if key.vk == libtcod.KEY_UP:
+        return {'move': (0,-1)}
+    elif key.vk == libtcod.KEY_DOWN:
+        return {'move': (0,1)}
+    elif key.vk == libtcod.KEY_LEFT:
+        return {'move': (-1,0)}
+    elif key.vk == libtcod.KEY_RIGHT:
+        return {'move': (1,0)}
+
+    if key.vk == libtcod.KEY_ENTER and key.lalt:
+        #alt+enter: toggle full screen
+        return {'fullscreen': True}
+    elif key.vk == libtcod.KEY_ESCAPE:
+        #exit the game
+        return {'exit': true}
+    #no key was pressed
+    return {}
